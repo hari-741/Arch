a2d11f490f315fb99e953dc7a2890da1e82dba59
Hariprasaad <1hariprasaad@gmail.com>
2024-09-22 20:10:24 +0530

    Update mimeapps.list and alacritty.toml

--- packages.sh
+++ packages.sh
@@ -5,15 +5,15 @@
 # systemctl enable NetworkManager
 # systemctl enable sddm
 
-sudo pacman -S alacritty bat bleachbit brightnessctl btop blueman bluez bluez-utils chromium cups cups-pdf dialog dunst exfatprogs eza github-cli galculator gvfs gvfs-mtp guvcview hplip hyprland hyprpaper hypridle hyprlock libreoffice-fresh intel-media-driver imv less mpv neofetch ntfs-3g nvidia nwg-look pavucontrol pipewire-alsa pipewire-pulse  papirus-icon-theme polkit-gnome qt5-wayland qt5ct reflector sbctl starship system-config-printer thunar thunar-volman transmission-qt tldr ttf-fira-code tumbler unrar ufw unzip vulkan-intel waybar xdg-desktop-portal-hyprland xdg-utils xorg-xhost zathura zathura-pdf-poppler zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
+sudo pacman -S alacritty bat bleachbit brightnessctl btop blueman bluez bluez-utils dialog dunst exfatprogs eza github-cli galculator gvfs gvfs-mtp guvcview hyprland hyprpaper hypridle hyprlock libreoffice-fresh intel-media-driver imv less mpv fastfetch ntfs-3g nwg-look pavucontrol pipewire-alsa pipewire-pulse  papirus-icon-theme polkit-gnome qt5-wayland qt5ct reflector starship thunar thunar-volman transmission-qt tldr ttf-fira-code tumbler unrar ufw unzip vulkan-intel waybar xdg-desktop-portal-hyprland xdg-utils xorg-xhost zathura zathura-pdf-poppler zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
 
-# sudo pacman -S alsa-utils clamav engrampa gparted grub-customizer gufw plymouth  qbittorrent thunar-archive-plugin timeshift tlp tlp-rdw waybar zram-generator zsh-theme-powerlevel10k
+# sudo pacman -S alsa-utils clamav cups cups-pdf engrampa gparted grub-customizer gufw hplip nvidia plymouth qbittorrent sbctl system-config-printer thunar-archive-plugin timeshift tlp tlp-rdw zram-generator zsh-theme-powerlevel10k
 
-sudo pacman -S virt-manager virt-viewer qemu vde2 ebtables iptables-nft nftables dnsmasq bridge-utils ovmf freerdp
+# sudo pacman -S virt-manager virt-viewer qemu vde2 ebtables iptables-nft nftables dnsmasq bridge-utils ovmf freerdp
 
-yay -S auto-cpufreq bibata-cursor-theme catppuccin-gtk-theme-macchiato envycontrol hyprshot rofi-lbonn-wayland-git sddm-sugar-candy-git visual-studio-code-bin 
+yay -S auto-cpufreq bibata-cursor-theme brave-bin catppuccin-gtk-theme-macchiato hyprshot rofi-lbonn-wayland-git visual-studio-code-bin 
 
-# yay -S preload wlogout
+# yay -S envycontrol preload sddm-sugar-candy-git wlogout
 
 # sudo systemctl enable auto-cpufreq
 # sudo systemctl enable clamav-freshclam-once.timer
@@ -26,5 +26,3 @@ yay -S auto-cpufreq bibata-cursor-theme catppuccin-gtk-theme-macchiato envycontr
 # sudo systemctl start libvirtd
 
 # libvirt input wheel lp autologin hari power video storage audio
-
-# curl -fsSL https://bun.sh/install | bash
